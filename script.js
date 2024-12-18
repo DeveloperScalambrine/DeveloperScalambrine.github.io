cc74eba026d0cace3215b458d763351a87faba4d
CARLOS HENRIQUE SCALAMBRINE DE SOUZA <135667372+DeveloperScalambrine@users.noreply.github.com>
2024-12-17 23:05:01 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-const menuToggle = document.getElementById('menu-toggle');
-const fullscreenMenu = document.getElementById('fullscreen-menu');
 
-menuToggle.addEventListener('click', () => {
-    menuToggle.classList.toggle('active');
-    fullscreenMenu.classList.toggle('hidden');
-});
