31f25da7d753b1131904dc3d961057d95203d194
CARLOS HENRIQUE SCALAMBRINE DE SOUZA <135667372+DeveloperScalambrine@users.noreply.github.com>
2024-12-28 13:01:32 -0300

    alter logic js

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const menuClose = document.getElementById('menu-close');
 const fullscreenMenu = document.getElementById('fullscreen-menu');
 const menuLinks = document.querySelectorAll('.menu-link');
 const sections = document.querySelectorAll('.content-section');
+const section = document.getElementById('sectionId');
 
 window.onload = function () {
     alert("Portfólio em atualização");
@@ -22,9 +23,12 @@ menuClose.addEventListener('click', () => {
 menuLinks.forEach(link => {
     link.addEventListener('click', event => {
         event.preventDefault();
-        const targetSection = document.querySelector(link.getAttribute('href')); // Usa o href diretamente
-        sections.forEach(section => section.classList.remove('visible')); // Remove visibilidade de todas as seções
-        if (targetSection) targetSection.classList.add('visible'); // Adiciona visibilidade à seção alvo
-        fullscreenMenu.classList.remove('visible'); // Fecha o menu
+        const sectionId = link.dataset.section;
+         section.scrollIntoView({ behavior: 'smooth' });
+
+       // const targetSection = document.querySelector(link.getAttribute('href')); // Usa o href diretamente
+ //       sections.forEach(section => section.classList.remove('visible')); // Remove visibilidade de todas as seções
+   //     if (targetSection) targetSection.classList.add('visible'); // Adiciona visibilidade à seção alvo
+     //   fullscreenMenu.classList.remove('visible'); // Fecha o menu
     });
 });
