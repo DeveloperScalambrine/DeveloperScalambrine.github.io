f8301550e5a73c4eeda5c43bd4f74b23f6e15f35
CARLOS HENRIQUE SCALAMBRINE DE SOUZA <135667372+DeveloperScalambrine@users.noreply.github.com>
2024-12-30 04:35:42 -0300

    Update script.js

--- script.js
+++ script.js
@@ -96,7 +96,7 @@ dropdownItems.forEach(item => {
                 carouselItem.className = 'carousel-item';
                 carouselItem.innerHTML = `
                 
-                    <img src="${project.image}" class="img-fluid" alt="${project.title}">
+                    <img src="${project.image}" alt="${project.title}">
                     <div class="carousel-caption d-none d-md-block">
                         <p>"${project.description}"</p>
                         <a href="${project.url}" class="btn btn-primary btn-sm" target="_blank">Ver Projeto</a>     
