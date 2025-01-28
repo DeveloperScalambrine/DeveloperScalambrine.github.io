c253d24588af1cf5661ee5fb7a580f4ff90c155c
CARLOS HENRIQUE SCALAMBRINE DE SOUZA <135667372+DeveloperScalambrine@users.noreply.github.com>
2025-01-28 05:11:14 -0300

    Update script.js

--- script.js
+++ script.js
@@ -140,7 +140,8 @@ dropdownItems.forEach(item => {
 });
 
 const desafios = [
-    "Você é um analista de dados júnior que trabalha na equipe de analistas de marketing da Cyclistic, uma empresa de compartilhamento de bicicletas em Chicago. O diretor de marketing acredita que o sucesso futuro da empresa depende da maximização do número de planos anuais contratados. Portanto, sua equipe quer entender como os ciclistas casuais e os membros anuais usam as bicicletas da Cyclistic de forma diferente. A partir desses insights, sua equipe criará uma nova estratégia de marketing para converter passageiros casuais em membros anuais. Mas, primeiro, os executivos da Cyclistic devem aprovar suas recomendações que, portanto, devem ser apoiadas com insights de dados convincentes e visualizações de dados profissionais.",
+    "Como analista júnior na Cyclistic, você precisa identificar as diferenças de uso entre clientes casuais e membros anuais.\
+     O objetivo é desenvolver uma estratégia de marketing eficaz para converter mais clientes casuais em assinantes anuais."  
     "Busco resolver problemas complexos com soluções eficientes e escaláveis.",
     "Aprendendo novas ferramentas para aprimorar minhas habilidades em BI.",
     "Explorando machine learning para análises mais avançadas.",
